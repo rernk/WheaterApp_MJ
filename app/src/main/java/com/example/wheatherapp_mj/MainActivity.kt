4b31da91259475157d0b898ca563c2b57a5ff7b5
mjuracka <mjuracka@microstep-hdo.sk>
2021-01-21 15:43:38 +0100

    Wheather App.Added new theme for main screen.

--- app/src/main/java/com/example/wheatherapp_mj/MainActivity.kt
+++ app/src/main/java/com/example/wheatherapp_mj/MainActivity.kt
@@ -1,12 +1,164 @@
 package com.example.wheatherapp_mj
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    //Const vals
+    val PERMISSON_REQUEST_CODE = 1101
+    val PLAY_SERVICE_RESOLUTIONS_REQUEST = 1000
+
+    //Variables
+    var mGoogleApiClient: GoogleApiClient? = null
+    var mLocationRequest: LocationRequest? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        requestPermission();
+        if (checkPlayService())
+        {
+            buildGoogleApiClient()
+        }
     }
+
+    private fun requestPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            requestPermissions(
+                arrayOf(
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ), PERMISSON_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSON_REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayService()) {
+                        buildGoogleApiClient()
+                    }
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this).addApi(LocationServices.API).build()
+    }
+
+    private fun checkPlayService(): Boolean {
+        var resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(
+                    resultCode,
+                    this,
+                    PLAY_SERVICE_RESOLUTIONS_REQUEST
+                ).show()
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "This device is not more supported",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+            return false
+        }
+        return true
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        createLocationRequest();
+    }
+
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+        mLocationRequest!!.interval = 10000 //10 sec
+        mLocationRequest!!.fastestInterval = 3000 //3 sec
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+            mGoogleApiClient,
+            mLocationRequest,
+            this
+        )
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        mGoogleApiClient!!.connect()
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.i("ERROR", "Connection faied")
+    }
+
+    override fun onLocationChanged(location: Location) {
+
+        txtLocation.text = "${location!!.latitude} - ${location!!.longitude}"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mGoogleApiClient != null)
+        {
+            mGoogleApiClient!!.connect()
+        }
+    }
+
+    override fun onDestroy() {
+        mGoogleApiClient!!.disconnect()
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkPlayService()
+    }
+
+
 }
